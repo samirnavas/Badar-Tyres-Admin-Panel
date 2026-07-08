450aa72605e6f5481c82da44c4ca4ed620688fd3
Samir Navas <samirnavas7662@gmail.com>
2026-07-08 14:02:19 +0530

    pwa icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,19 +15,19 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ["business", "productivity"],
     icons: [
       {
-        src: "/pwa/icon-192.png",
+        src: "/pwa/icon-192-v2.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/pwa/icon-512.png",
+        src: "/pwa/icon-512-v2.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/pwa/icon-maskable-512.png",
+        src: "/pwa/icon-maskable-512-v2.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
